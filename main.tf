d61ab8985c63a0474e40ecf75d7a34a93c00f24b
wongmeng99 <wongmeng99@gmail.com>
2023-03-30 15:31:16 +0800

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "test_henry_wong"
 
     workspaces {
       name = "learn-terraform-github-actions"
